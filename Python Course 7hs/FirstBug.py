c70f85a81478ce3db065bb1ab051f3d030df1276
Jin Ozono <ozononicholas@gmail.com>
2025-02-03 12:22:11 -0300

    initial commit

---
+++ Python Course 7hs/FirstBug.py
@@ -0,0 +1,7 @@
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+print(f"Player's health is: {player_health}")
+print(f"Sword's damage is: {sword_damage}")
+print(f"Player's health after attack is: {health_after_attack}")
